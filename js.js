3c12042a6513fcc3eb1a24ad8c22a840e252fbfe
AVolosciuc <anea.tanas@mail.ru>
2022-11-10 17:52:43 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,42 @@
+function convert(params) {
+    let celsius = params;
+    let farenheit = (celsius * 1.8) + 32;
+    console.log(farenheit);
+}
+
+convert(60);
+
+function aria(latime,lungime) {
+    let aria=latime*lungime;
+    console.log(aria);
+}
+
+aria(2,5);
+
+function reversed(number)
+{
+    let reversNumber= Number(String(number).split('').reverse().join(''));
+
+    console.log(reversNumber);
+}
+
+reversed(1053);
+
+function vowelCount(str) {
+    let vowelsCount1 = 0;
+    const vowels = ['a','e','i','o','u'];
+    for(let char of str)
+    {
+        if (vowels.includes(char)) {
+            vowelsCount1++;
+        }
+    }
+    console.log(vowelsCount1);
+}
+
+vowelCount("");
+
+
+function name(params) {
+    
+}
